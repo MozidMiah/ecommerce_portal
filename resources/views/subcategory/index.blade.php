792e14d29c636fd37f10d47846793aa8ab065dab
MozidMiah <mazidkhan278@gmail.com>
2026-08-13 22:17:44 +0600

    Subcategory

--- resources/views/subcategory/index.blade.php
+++ resources/views/subcategory/index.blade.php
@@ -14,86 +14,11 @@
                 <div class="col-lg-3 mb-4">
                     <div class="card">
                         <a href="#">
-                            <img src="{{ asset('assets/images/products/1.jpg') }}" class="card-img-top" alt="Product 1">
-                        </a>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><a href="#" class="text-decoration-none text-dark">Case shoe</a></h5>
-                            <p class="card-text text-center">$49.99</p>
-                            <div class="d-grid gap-2">
-                                <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-4">
-                    <div class="card">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/products/2.jpg') }}" class="card-img-top" alt="Product 1">
+                            <img src="{{ asset('assets/images/products/7.jpg') }}" class="card-img-top" alt="Product 1">
                         </a>
                         <div class="card-body">
                             <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Men's Watch</a></h5>
-                            <p class="card-text text-center">$549.99</p>
-                            <div class="d-grid gap-2">
-                                <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/products/3.jpg') }}" class="card-img-top" alt="Product 1">
-                        </a>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Hat</a></h5>
-                            <p class="card-text text-center">$149.99</p>
-                            <div class="d-grid gap-2">
-                                <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/products/4.jpg') }}" class="card-img-top" alt="Product 1">
-                        </a>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Wooden Chair</a></h5>
-                            <p class="card-text text-center">$249.99</p>
-                            <div class="d-grid gap-2">
-                                <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/products/5.jpg') }}" class="card-img-top" alt="Product 1">
-                        </a>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Digital Camera</a></h5>
-                            <p class="card-text text-center">$249.99</p>
-                            <div class="d-grid gap-2">
-                                <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/products/6.jpg') }}" class="card-img-top" alt="Product 1">
-                        </a>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Case shoe</a></h5>
+                                    class="text-decoration-none text-dark">MI LED TV</a></h5>
                             <p class="card-text text-center">$249.99</p>
                             <div class="d-grid gap-2">
                                 <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
@@ -108,8 +33,8 @@
                         </a>
                         <div class="card-body">
                             <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">LED TV</a></h5>
-                            <p class="card-text text-center">$249.99</p>
+                                    class="text-decoration-none text-dark">Samsung LED TV</a></h5>
+                            <p class="card-text text-center">$5449.99</p>
                             <div class="d-grid gap-2">
                                 <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
                             </div>
@@ -119,12 +44,12 @@
                 <div class="col-lg-3 mb-4">
                     <div class="card">
                         <a href="#">
-                            <img src="{{ asset('assets/images/products/8.jpg') }}" class="card-img-top" alt="Product 1">
+                            <img src="{{ asset('assets/images/products/7.jpg') }}" class="card-img-top" alt="Product 1">
                         </a>
                         <div class="card-body">
                             <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Washing Machine</a></h5>
-                            <p class="card-text text-center">$249.99</p>
+                                    class="text-decoration-none text-dark">Sony LED TV</a></h5>
+                            <p class="card-text text-center">$8449.99</p>
                             <div class="d-grid gap-2">
                                 <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
                             </div>
@@ -134,27 +59,12 @@
                 <div class="col-lg-3 mb-4">
                     <div class="card">
                         <a href="#">
-                            <img src="{{ asset('assets/images/products/9.jpg') }}" class="card-img-top" alt="Product 1">
+                            <img src="{{ asset('assets/images/products/7.jpg') }}" class="card-img-top" alt="Product 1">
                         </a>
                         <div class="card-body">
                             <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Ladies Bag</a></h5>
-                            <p class="card-text text-center">$249.99</p>
-                            <div class="d-grid gap-2">
-                                <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/products/10.jpg') }}" class="card-img-top" alt="Product 1">
-                        </a>
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><a href="#"
-                                    class="text-decoration-none text-dark">Travel Bag</a></h5>
-                            <p class="card-text text-center">$249.99</p>
+                                    class="text-decoration-none text-dark">LG LED TV</a></h5>
+                            <p class="card-text text-center">$3449.99</p>
                             <div class="d-grid gap-2">
                                 <a href="#" class="btn theme-green-btn btn-sm text-light">Add to Cart</a>
                             </div>
